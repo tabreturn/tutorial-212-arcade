909938996c78ced4eca22c268e1a6099600b18ec
tabreturn <github@tabreturn.com>
2018-09-11 14:02:50 +1200

    - amend js file (fix warnings)

--- week_01/static/js.js
+++ week_01/static/js.js
@@ -1,28 +1,28 @@
 // variables
 
-colors = [
+var colors = [
   '#D00',
   '#DD0',
   '#0DD',
   '#22D',
   '#D0D'
-]
+];
 
-scores = [
+var scores = [
  /* ['TAB','1991-03-03','12345'] */
-]
+];
 
-topscore = 15000;
+var topscore = 15000;
 
 // insert entry
 
-function insertEntry() {
+function insertEntry(entry) {
   scores.push(entry);
 
   document.getElementById('highscores').innerHTML = '<tr><th>[NAME]</th><th>[DATE]</th><th>[SCORE]</th></tr>';
   document.getElementById('chart').innerHTML = '';
 
-  for (i in scores) {
+  for (var i=0; i < scores.length; i++) {
     color = colors[i%colors.length];
 
     var bar = document.createElement('div');
@@ -43,11 +43,11 @@ function insertEntry() {
 // submit button
 
 document.querySelector('#addscore a').addEventListener('click', function() {
-  entry = [
+  var entry = [
    document.getElementById('name').value,
    document.getElementById('date').value,
    document.getElementById('score').value
-  ]
+  ];
 
   document.querySelector('#addscore .alert').innerHTML = '';
 
@@ -55,12 +55,12 @@ document.querySelector('#addscore a').addEventListener('click', function() {
     document.querySelector('#addscore .alert').innerHTML = 'RE-ENTER NAME!';
   }
   else if (entry[1].search(/[0-9]{4}-[0-9]{2}-[0-9]{2}/) == -1) {
-    document.querySelector('#addscore .alert').innerHTML = 'MISSING DATE!';
+    document.querySelector('#addscore .alert').innerHTML = 'RE-ENTER DATE!';
   }
-  else if (entry[2].search(/^[0-9]*$/) < 0) {
-    document.querySelector('#addscore .alert').innerHTML = 'MISSING SCORE!';
+  else if (entry[2].search(/^[0-9]*$/) < 0 || entry[2] == '') {
+    document.querySelector('#addscore .alert').innerHTML = 'RE-ENTER SCORE!';
   }
   else {
-    insertEntry()
+    insertEntry(entry);
   }
-})
+});
